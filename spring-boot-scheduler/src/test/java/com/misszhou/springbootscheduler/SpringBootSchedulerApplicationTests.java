c9c229cd59df0af17ef11d988dd041ccffc454d9
misszhou <414600762@qq.com>
2023-08-02 23:28:49 +0800

    spring boot 3.0.2

---
+++ spring-boot-scheduler/src/test/java/com/misszhou/springbootscheduler/SpringBootSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misszhou.springbootscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
